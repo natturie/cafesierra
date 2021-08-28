e35bce752ef3e727ee06e612f0a3144213351d0d
natturie <xnrodriguezg@unal.edu.co>
2021-08-27 21:10:49 -0500

    Iniciando proyecto

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="menu">
+
+<aside class="side-menu">
+    <div >
+        <img class="logo" src="../assets/logo.png" alt="logo sierra">
+    </div>
+    <nav class="navegacion">
+        <li><a>Clientes</a></li>
+        <li><a>Productos</a></li>
+        <li><a>Pedidos</a></li>
+    </nav>
+    </aside> 
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.side-menu{
+    background: #224957;
+    width: 20%;
+    height: 100vh;
+    display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+	justify-content: flex-start;
+	align-items: center;
+	align-content: center;
+}
+
+.logo{
+    width: 100px;
+    padding: 20px 0;
+}
+
+.navegacion li {
+    width: 80%;
+    border: blueviolet 2px solid;
+    list-style: none;
+    padding: 10px;
+    margin: 20px;
+
+}
+
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="productos">
+    <sidebar msg="sidebar" />
+    <div class="tabla-product">
+        <h3>Productos</h3>
+        <button>Nuevo producto</button>
+      <table>
+        <thead>
+            <tr>
+          <th>Nombre</th>
+          <th>Categoria</th>
+          <th>Cantidad</th>
+          <th>Precio</th>
+          <th>Editar</th>
+          <th>Elimnar</th>
+          </tr>
+        </thead>
+        <tbody>
+
+            <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td><button>A</button></td>
+            <td><button>B</button></td>
+            </tr>
+
+            <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td><button>A</button></td>
+            <td><button>B</button></td>
+            </tr>
+
+            <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td><button>A</button></td>
+            <td><button>B</button></td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "../components/Sidebar.vue";
+export default {
+  name: "Products",
+  components: {
+    Sidebar,
+  },
+};
+</script>
+<style scoped>
+.tabla-product {
+  background: orangered;
+  width: 40%;
+  
+}
+</style>
