5234d0ea5bcaf0c97e19f50f2e248f19326c3d1a
Luis Palacio <luispalacio11c@gmail.com>
2021-09-01 15:48:17 -0500

    agregando apollo client

--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
+import { setContext } from 'apollo-link-context'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
 
-createApp(App).use(router).mount('#app')
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+    uri: 'https://apigatewaycafe.herokuapp.com/',
+})
+
+const authLink = setContext((request, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            'Authorization': localStorage.getItem('token') || ''
+        }
+    }
+})
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+})
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+})
+createApp(App).use(router).use(apolloProvider).mount('#app')
